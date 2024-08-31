40224b3d341dc1ff2d943f2499d08ca5a80150e8
yacin-hamdi <yacin.ha9@gmail.com>
2024-08-31 12:53:02 +0100

    add dependency injection for ImageRepository

--- app/src/main/java/ym/tutorials/eesobcamera/di/AppModule.kt
+++ app/src/main/java/ym/tutorials/eesobcamera/di/AppModule.kt
@@ -7,7 +7,9 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import ym.tutorials.eesobcamera.data.manager.objectDetection.ObjectDetectionManagerImpl
+import ym.tutorials.eesobcamera.data.repository.ImageRepositoryImpl
 import ym.tutorials.eesobcamera.domain.manager.objectDetection.ObjectDetectionManager
+import ym.tutorials.eesobcamera.domain.repository.ImageRepository
 import javax.inject.Singleton
 
 @Module
@@ -20,5 +22,13 @@ object AppModule {
         @ApplicationContext context: Context
     ): ObjectDetectionManager {
         return ObjectDetectionManagerImpl(context)
+    }
+
+    @Provides
+    @Singleton
+    fun providesImageRepository(
+        @ApplicationContext context: Context
+    ): ImageRepository{
+        return ImageRepositoryImpl(context)
     }
 }
